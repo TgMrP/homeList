84ed0c828a398715e7e218e021142d774bc67a67
Ran Tayar <tgmrp.dev@gmail.com>
2021-02-22 16:04:52 +0200

    add list of items ind board view

---
+++ resources/js/Components/listItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div
+      class="card shadow-lg compact side bg-primary"
+      v-for="item in items"
+      :key="item.id"
+    >
+      <div class="flex-row items-center space-x-4 card-body">
+        <div>
+          <h2 class="card-title text-content-primary">{{ item.title }}</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- resources/js/Pages/Board.vue
+++ resources/js/Pages/Board.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    {{ board }}
+  <div class="container mx-auto">
+    <div class="title py-4 prose text-center w-full">
+      <h1>{{ board.title }}</h1>
+    </div>
+    <div>
+      <list-item :items="board.items" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4" />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import listItem from '../Components/listItem.vue';
 
 export default {
+  components: { listItem },
   computed: {
     ...mapGetters(['Boards/getBoard']),
   },
